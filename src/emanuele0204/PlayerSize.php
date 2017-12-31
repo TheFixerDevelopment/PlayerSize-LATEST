c9e68d3e3099eb337d7b1cd9797c3bdf5755053a
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-12-31 18:31:37 +0000

    Update PlayerSize.php

--- src/emanuele0204/PlayerSize.php
+++ src/emanuele0204/PlayerSize.php
@@ -2,27 +2,9 @@
 
 namespace emanuele0204\PlayerSize;
 
-use pocketmine\plugin\PluginBase;
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\entity\Entity;
 use pocketmine\{Server, Player};
-
-class PlayerSize extends PluginBase{
-    
-    public $b = array();
-    public function onEnable(){
-        $this->getLogger()->info("§8» §ePlayerSize active, loaded.");
-        $this->getServer()->getCommandMap()->register("size", new pSize($this));
-    }
-    
-    public function respawn(PlayerRespawnEvent $e){
-        $o = $e->getPlayer();
-        if(!empty($this->b[$o->getName()])){
-            $nomep = $this->b[$o->getName()];
-            $o->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $nomep);
-        }
-    }
-}
 
 class PlayerSize extends Command{
     
