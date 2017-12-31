6ab70daf8ffc81ae9437f5caf91929ff67488be9
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-12-31 18:32:39 +0000

    Renamed.

---
+++ src/emanuele0204/Loader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace emanuele0204\PlayerSize;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\entity\Entity;
+use pocketmine\{Server, Player};
+
+class Main extends PluginBase{
+    
+    public $b = array();
+    public function onEnable(){
+        $this->getLogger()->info("§8» §ePlayerSize active, loaded.");
+        $this->getServer()->getCommandMap()->register("size", new pSize($this));
+    }
+    
+    public function respawn(PlayerRespawnEvent $e){
+        $o = $e->getPlayer();
+        if(!empty($this->b[$o->getName()])){
+            $nomep = $this->b[$o->getName()];
+            $o->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $nomep);
+        }
+    }
+}
+
+      }
+   }
+}
